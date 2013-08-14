0af292f6bc45d67f1b22d5a54179401d1c9ff12c
Jylia Grigorchuk <grigorchukj@gmail.com>
2013-08-14 22:21:36 +0300

    problem_2 var. 2

--- projectEuler_2.rb
+++ projectEuler_2.rb
@@ -1,22 +1,21 @@
 def Fibonacci(start, second, limit)
   sum = 0
-  if second % 2 == 0 then
-    sum += second
-  end
-  if start % 2 == 0 then
-    sum += start
+  a = [start, second, start + second]
+  a.each do |ach| 
+    if ach % 2 == 0 then
+	  sum += ach
+	end
   end	
-  i = start
-  loop do
-  third = start + second
-  if third % 2 == 0 then
-	sum += third
-  end
-  start = second
-  second = third
-  if third > limit then break end 
-  end
 
+  loop do
+    third = start + second
+    if third % 2 == 0 then
+      sum += third
+	end
+    start = second
+    second = third
+    if third > limit then break end 
+  end
   return sum  
 end
 
