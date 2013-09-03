f1d4a2f64ced37c8838206d2de968d06ecc5b690
Jylia Grigorchuk <grigorchukj@gmail.com>
2013-09-03 23:27:09 +0300

    update task2

--- projectEuler_2.rb
+++ projectEuler_2.rb
@@ -1,23 +1,11 @@
-def Fibonacci(start, second, limit)
+  a = [0, 1]
   sum = 0
-  a = [start, second, start + second]
-  a.each do |ach| 
-    if ach % 2 == 0 then
-	  sum += ach
-	end
-  end	
-
   loop do
-    third = start + second
-    if third % 2 == 0 then
-      sum += third
-	end
-    start = second
-    second = third
-    if third > limit then break end 
+    break if (a[0] + a[1] > 4000000) 
+    third = a[0] + a[1]
+	sum += third if third % 2 == 0
+	a[0] = a[1]
+    a[1] = third  
   end
-  return sum  
-end
-
-puts Fibonacci(1, 2, 4000000)
+  puts sum
 
