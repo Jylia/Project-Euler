7227096e4d01ec8d7f0e39ec788c4cb836692f72
Julia Grigorchuk <mac@imac-mac.dlink>
2014-01-11 02:34:56 +0200

    Test solve problem 17

--- projectEuler_17.rb
+++ projectEuler_17.rb
@@ -19,31 +19,45 @@ def words
       array.push([number_to_words(number.divmod(10).first*10), number_to_words(number % 10)])
     end 
     
-    if (number == 100)
-      array.push('one hundred')
-    end 
-    
-    if (number > 100) && (number < 200)
-      array.push([number_to_words(number.divmod(100).first), 'hundred and ', number_to_words(number.divmod(10).first%10*10), number_to_words(number.divmod(100).last%10)])
-    end	
-
-    if (number == 200)||(number == 300)||(number == 400)||(number == 500)||(number == 600)||(number == 700)||(number == 800)||(number == 900)
-      array.push([number_to_words(number.divmod(100).first), 'hundreds'])
+    if (number == 100)||(number == 200)||(number == 300)||(number == 400)||(number == 500)||(number == 600)||(number == 700)||(number == 800)||(number == 900)
+      array.push([number_to_words(number.divmod(100).first), 'hundred'])
     end 
 
-    if (number > 200) && (number < 1000) && (number != 300) && (number != 400) && (number != 500) && (number != 600) && (number != 700) && (number != 800) && (number != 900)
-      array.push([number_to_words(number.divmod(100).first), 'hundreds and ', number_to_words(number.divmod(10).first%10*10), number_to_words(number.divmod(100).last%10)])
-    end	
+    if (number > 100) && (number < 1000) && (number != 100) && (number != 200) && (number != 300) && (number != 400) && (number != 500) && (number != 600) && (number != 700) && (number != 800) && (number != 900)
+      if number.divmod(100).last < 21
+        array.push([number_to_words(number.divmod(100).first), 'hundredand', number_to_words(number.divmod(100).last)])
+      else
+        array.push([number_to_words(number.divmod(100).first), 'hundredand', number_to_words(number.divmod(100).last.divmod(10).first*10), number_to_words(number.divmod(100).last.divmod(10).last)])
+      end  
+    end	   
 
     if (number == 1000)
       array.push('one thousand')
     end	
   end
-
-  array.each do |elem|
-    puts elem.chars.count 
+  counter = 0
+  new_array = []
+  # puts array.to_s.split(//).gsub!(/\W+/, '')
+  array.to_s.scan(/[[:print:]]/).join.split(//).reject { |s| /[^a-zA-Z]/ =~ s }.each do |elem|
+    # elem = elem.gsub(/^[a-zA-Z]+$/, '')
+    # elem =elem.gsub(/[\s+)(]/,"")
+    new_array = new_array.push(elem) #if elem != ''
+    # puts elem.gsub(/\W+/, '').gsub(/[\s+)(]/,"").split(//)
+    # counter = counter + 1 
   end
+  # puts new_array
+  puts new_array.count
+  # puts new_array
+  puts 7*9+19+17
+  # new_array.each do |elem|
+  #   # new_array = new_array.push(elem.gsub(/\W+/, '').gsub(/[\s+)(]/,"").split(//))
+  #   # puts elem.gsub(/\W+/, '').gsub(/[\s+)(]/,"").split(//)
+  #   counter = counter + 1 
+  #   puts elem
+  # end
+
+  # puts counter
 end	
 
 words
-
+# puts 123.divmod(100).last
