ae25665c4437b16069b9ec7690680a442e8567ad
Julia Grigorchuk <julia.grigorchuk@anahoret.com>
2014-03-12 10:34:57 +0200

    Problem 15

---
+++ projectEuler_15.rb
@@ -0,0 +1,18 @@
+#Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+#How many such routes are there through a 20×20 grid?
+
+def right x, y
+  x = x + 1
+end
+
+def down x, y
+  y = y + 1
+end
+
+def moving size, start_point
+  while x < size do
+    while y < size do
+
+    end
+  end
+end
